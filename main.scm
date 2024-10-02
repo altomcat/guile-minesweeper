b94489f0b5ab685ca53d220f8041b84597117391
Arnaud Lechevallier <arnaud.lechevallier@free.fr>
2024-10-02 21:46:56 +0200

    Added max mines and max flags inside the game record.

--- main.scm
+++ main.scm
@@ -4,22 +4,26 @@
 
 (define ROWS 10)
 (define COLS 10)
+(define MINES 10)
+(define FLAGS 3)
 
 (gui:init-raylib ROWS COLS)
 
-(define (run game acc)
-  (let* ((minefield (minefield-random ROWS COLS 10))
+(define (run-game rows cols mines flags acc)
+  (let* ((minefield (minefield-random rows cols mines flags))
+	 (game (gui:init-game rows cols))
 	 (intro (and (= acc 0)
 		     (gui:intro-game game)))
 	 (result (begin
-		   ;; define the number of available flags
-		   (set-minefield-max-flags! minefield 3)
+		   ;; define the number of available flags and mines
+                   (set-minefield-max-flags! minefield flags)
+		   (set-minefield-max-mines! minefield mines)
 		   (gui:main-game game minefield))))
     (display (gui:minesweeper-game-clock game))
     (gui:end-game game)
     (when (equal? result 'restart)
-      (run (gui:init-game ROWS COLS) (1+ acc)))))
+      (run-game rows cols mines flags (1+ acc)))))
 
 (define start
-  (and (run (gui:init-game ROWS COLS) 0)
+  (and (run-game ROWS COLS MINES FLAGS 0)
        (gui:end-raylib)))
